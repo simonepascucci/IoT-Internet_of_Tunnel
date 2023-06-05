043427c397ca254c24ccf529c2af81258c27d164
Gaggo00 <gavrieldnp@gmail.com>
2023-06-05 18:13:26 +0200

    cerchi colorati con i valori

--- webapp/index.js
+++ webapp/index.js
@@ -19,12 +19,14 @@ function callAPI() {
 
 function storeData(data) {
   console.log(data);
-  document.getElementById("AQ1").innerText =
-    "Air Quality sensor 1: " + data["AQ1"];
-  document.getElementById("AQ2").innerText =
-    "Air Quality sensor 2: " + data["AQ2"];
-  document.getElementById("Lux1").innerText = "Light level 1: " + data["Lux1"];
-  document.getElementById("Lux2").innerText = "Light level 2: " + data["Lux2"];
+  document.getElementById("AQ1").innerText = data["AQ1"];
+  setColorAirQuality("square1", data["AQ1"]);
+  document.getElementById("AQ2").innerText = data["AQ2"];
+  setColorAirQuality("square2", data["AQ2"]);
+  document.getElementById("Lux1").innerText = data["Lux1"];
+  setColorLux("square3", data["Lux1"]);
+  document.getElementById("Lux2").innerText = data["Lux2"];
+  setColorLux("square4", data["Lux1"]);
   document.getElementById("AQ1s").innerText =
     "Air Quality 1 Status: " + data["Status AQ1"];
   document.getElementById("AQ2s").innerText =
@@ -36,6 +38,30 @@ function storeData(data) {
   document.getElementById("ts").innerText =
     "Measurement taken at: " + data["timestamp"];
 }
+
+function setColorAirQuality(squareId, value) {
+  var square = document.getElementById(squareId);
+
+  if (value < 1100) {
+    square.className = 'square green';
+  } else if (value < 1300) {
+    square.className = 'square orange';
+  } else {
+    square.className = 'square red';
+  }
+}
+function setColorLux(squareId, value) {
+  var square = document.getElementById(squareId);
+
+  if (value < 30) {
+    square.className = 'square red';
+  } else if (value < 70) {
+    square.className = 'square orange';
+  } else {
+    square.className = 'square green';
+  }
+}
+
 
 function init() {
   callAPI();
